e52e0d6ae67b8a5741c691dafd4258fa7c3b894e
Alishba-Bacha <130219545+Alishba-Bacha@users.noreply.github.com>
2023-06-21 16:26:47 +0500

    1

---
+++ structures.cpp
@@ -0,0 +1,32 @@
+//Academic Information System based on structures
+#include<iostream>
+using namespace std;
+
+//struct for course detail
+struct Course
+{
+
+};
+
+//A structure to controll all courses list
+struct Course_list
+{
+    
+};
+
+//Structure for student details
+struct Student
+{
+    
+};
+
+//Structure for management of student detail
+struct Student_List
+{
+    
+};
+
+int main()
+{
+    
+}
